8d0b7337a009a1855f715667c0d8e93dd9415ee2
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2023-08-28 19:46:45 -0300

    Support serving ogc-features from PostGIS
    * Running
    
    ```
    docker compose up -d
    ```
    
    Will launch the PostGIS container and the
    `ogc-features` application using PostGIS as its data source.
    
    In order to run the `ogc-features` service against PostGIS,
    it must be run with the `postgis` Spring profile,
    as well as the following environment variables with the
    appropriate values for the target environment:
    
    ```
    SPRING_PROFILES_ACTIVE: postgis
    POSTGRES_HOST: postgis
    POSTGRES_PORT: 5432
    POSTGRES_DB: postgis
    POSTGRES_SCHEMA: opendataindex
    POSTGRES_USER: postgis
    POSTGRES_PASSWORD: postgis
    POSTGRES_POOL_MAXSIZE: 20
    POSTGRES_POOL_MINSIZE: 0
    ```
    
    * Usage
    
    Browse to [http://localhost:8080](http://localhost:8080), will redirect
    to [http://localhost:8080/swagger-ui/index.html](http://localhost:8080/swagger-ui/index.html),
    where you'll find the Swagger UI for the API.
    
    * Sample data
    
    The same sample data used for development can be imported to
    PostGIS with the following command:
    
    ```
    docker compose exec postgis /import-sample-data.sh
    ```

--- src/services/ogc-features/src/main/java/com/camptocamp/opendata/ogc/features/app/OgcFeaturesApp.java
+++ src/services/ogc-features/src/main/java/com/camptocamp/opendata/ogc/features/app/OgcFeaturesApp.java
@@ -3,12 +3,16 @@ package com.camptocamp.opendata.ogc.features.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.camptocamp.opendata.ogc.features.autoconfigure.GeoToolsStaticContextInitializer;
+
 @SpringBootApplication
 public class OgcFeaturesApp {
 
     public static void main(String... args) {
         try {
-            SpringApplication.run(OgcFeaturesApp.class, args);
+            SpringApplication app = new SpringApplication(OgcFeaturesApp.class);
+            app.addInitializers(new GeoToolsStaticContextInitializer());
+            app.run(args);
         } catch (RuntimeException e) {
             e.printStackTrace();
             System.exit(-1);
---
+++ src/services/ogc-features/src/main/java/com/camptocamp/opendata/ogc/features/autoconfigure/GeoToolsStaticContextInitializer.java
@@ -0,0 +1,12 @@
+package com.camptocamp.opendata.ogc.features.autoconfigure;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class GeoToolsStaticContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        System.setProperty("org.geotools.referencing.forceXY", "true");
+    }
+}

c9a5c2e5f033481b19d6cc4c551d4c87d66cc0c6
Gabriel Roldan <gabriel.roldan@camptocamp.com>
2023-08-29 11:25:35 -0300

    Support specifying the output format of fetch features with a query parameter
    Add a servlet Filter that replaces the `Accept` request header by the MimeType of
    a matching collection items `MimeTypes` supported format, if the
    `f` query parameter matches a `MimeTypes#getShortName()` short
    name supported output format.
    
    E.g.: `GET /ogcapi/collections/{collectionId}/items?f=shapefile`
    matches `MimeTypes#SHAPEFILE` and the Accept header is set to
    `application/x-shapefile`, resulting in a download of the collection
    in shapefile format and not in an error.

--- src/services/ogc-features/src/main/java/com/camptocamp/opendata/ogc/features/http/codec/MimeTypes.java
+++ src/services/ogc-features/src/main/java/com/camptocamp/opendata/ogc/features/http/codec/MimeTypes.java
@@ -8,33 +8,57 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.util.MimeType;
 
 import lombok.Getter;
+import lombok.NonNull;
 
 public enum MimeTypes {
 
-    SHAPEFILE(new MimeType("application", "x-shapefile")) {
+    JSON(new MimeType("application", "json"), "json", "JSON") {
+        public @Override void addHeaders(String collectionId, HttpHeaders headers) {
+        }
+    },
+    GeoJSON(new MimeType("application", "geo+json"), "geojson", "GeoJSON") {
+        public @Override void addHeaders(String collectionId, HttpHeaders headers) {
+        }
+    },
+    SHAPEFILE(new MimeType("application", "x-shapefile"), "shapefile", "Esri Shapefile") {
         public @Override void addHeaders(String collectionId, HttpHeaders headers) {
             contentDisposition(collectionId, "shp.zip", headers);
         }
     },
-    CSV(new MimeType("text", "csv", StandardCharsets.UTF_8)) {
+    CSV(new MimeType("text", "csv", StandardCharsets.UTF_8), "csv", "Comma Separated Values") {
         public @Override void addHeaders(String collectionId, HttpHeaders headers) {
             contentDisposition(collectionId, "csv", headers);
         }
     },
-    OOXML(new MimeType("application", "vnd.openxmlformats-officedocument.spreadsheetml.sheet")) {
+    OOXML(new MimeType("application", "vnd.openxmlformats-officedocument.spreadsheetml.sheet"), "ooxml",
+            "Excel 2007 / OOXML") {
         public @Override void addHeaders(String collectionId, HttpHeaders headers) {
             contentDisposition(collectionId, "xlsx", headers);
         }
     };
 
-    private final @Getter MimeType mimeType;
+    private final @Getter @NonNull MimeType mimeType;
+    private final @Getter @NonNull String shortName;
+    private final @Getter @NonNull String displayName;
 
-    private MimeTypes(MimeType type) {
+    private MimeTypes(MimeType type, String shortName, String displayName) {
         this.mimeType = type;
+        this.shortName = shortName;
+        this.displayName = displayName;
     }
 
-    public static Optional<MimeTypes> find(MimeType contentType) {
+    public static Optional<MimeTypes> find(@NonNull MimeType contentType) {
         return Arrays.stream(values()).filter(m -> m.getMimeType().isCompatibleWith(contentType)).findFirst();
+    }
+
+    public static Optional<MimeTypes> find(@NonNull String mimeType) {
+        MimeType contentType = MimeType.valueOf(mimeType);
+        return find(contentType);
+    }
+
+    public static Optional<MimeTypes> findByShortName(String parameter) {
+        return Optional.ofNullable(parameter)
+                .flatMap(p -> Arrays.stream(values()).filter(m -> m.getShortName().equals(p)).findFirst());
     }
 
     public abstract void addHeaders(String collectionId, HttpHeaders headers);

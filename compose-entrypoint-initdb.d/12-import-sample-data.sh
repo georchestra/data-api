edf0594510f17672f0497187f1ce5fa8fa2b163a
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2023-09-12 11:25:06 +0200

    Update import script
    Import script should now use the geopackage dataset as input

--- compose-entrypoint-initdb.d/12-import-sample-data.sh
+++ compose-entrypoint-initdb.d/12-import-sample-data.sh
@@ -3,13 +3,17 @@
 set -e
 
 # Load sample data PostGIS into $POSTGRES_DB
-for csv in `ls /sample-data/*csv`
-do
-	echo "Importing sample dataset $csv into database $POSTGRES_DB as user $POSTGRES_USER"
-	ogr2ogr -f PostgreSQL PG:"host='localhost' active_schema='opendataindex' user='$POSTGRES_USER' dbname='$POSTGRES_DB' password='$POSTGRES_PASSWORD'" \
-	$csv -oo AUTODETECT_TYPE=YES \
-	-oo X_POSSIBLE_NAMES=Longitude,LON \
-	-oo Y_POSSIBLE_NAMES=Latitude,LAT \
-	-oo KEEP_GEOM_COLUMNS=NO
-done
+# for csv in `ls /sample-data/*csv`
+# do
+# 	echo "Importing sample dataset $csv into database $POSTGRES_DB as user $POSTGRES_USER"
+# 	ogr2ogr -f PostgreSQL PG:"host='localhost' active_schema='opendataindex' user='$POSTGRES_USER' dbname='$POSTGRES_DB' password='$POSTGRES_PASSWORD'" \
+# 	$csv -oo AUTODETECT_TYPE=YES \
+# 	-oo X_POSSIBLE_NAMES=Longitude,LON \
+# 	-oo Y_POSSIBLE_NAMES=Latitude,LAT \
+# 	-oo KEEP_GEOM_COLUMNS=NO
+# done
+
+sleep 2 
+echo "Importing sample datasets sample-datasets.gpkg into database $POSTGRES_DB as user $POSTGRES_USER"
+ogr2ogr -f PostgreSQL PG:"host='localhost' active_schema='opendataindex' user='$POSTGRES_USER' dbname='$POSTGRES_DB' password='$POSTGRES_PASSWORD'" /sample-data/sample-datasets.gpkg
 
